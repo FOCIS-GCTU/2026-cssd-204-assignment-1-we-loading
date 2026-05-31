6528c14736e72f4a539e781cabbe88b5743b3bb8
we-loading <Kuburatuk@gmail.com>
2026-05-31 16:36:27 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,35 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Yakubu Suhuyeni
+# STUDENT ID: 2425403511
+# Email: kuburatuk@gmail.com
+# Grader: Mr Augustus Buckman
+#
+# On my honor, Yakubu Suhuyeni, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # Print my initials in large letters
 
+    print()
+    print("...YSY")
+    print()
+
+    print("...Y..........Y....SSSSSSSSSSSS........Y..........Y.....")
+    print("....Y........Y.....S....................Y........Y......")
+    print(".....Y......Y......S......................Y......Y......")
+    print("......Y....Y.......S.......................Y....Y.......")
+    print(".......Y  Y........SSSSSSSSSSSS.............Y..Y........")
+    print("........YY....................S..............YY.........")
+    print("........YY....................S..............YY.........")
+    print("........YY....................S..............YY.........")
+    print("........YY...**...S...........S...**.........YY.......**")
+    print("........YY...**...SSSSSSSSSSSS....**.........YY.......**")
+
+    print()
 
 
 main()
